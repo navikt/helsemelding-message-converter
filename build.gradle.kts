65c3109ac8705d0e28c1e20c92c6bb25b66dc1d4
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2026-08-06 11:34:59 +0200

    Lagt til withSourcesJar innstilling for prosjektet

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,12 +42,16 @@ plugins.withId("org.jlleitschuh.gradle.ktlint") {
     }
 }
 
+java {
+    withSourcesJar()
+}
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             groupId = "no.nav.helsemelding"
             artifactId = "message-converter"
-            version = "0.0.1"
+            version = "0.0.2-SNAPSHOT"
             from(components["java"])
         }
     }
