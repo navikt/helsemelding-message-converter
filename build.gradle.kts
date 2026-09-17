aef3967696dc61ac1d8279c5360f8fa4adbd2843
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2026-09-17 15:08:10 +0200

    Oppdatert message-converter til v0.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             groupId = "no.nav.helsemelding"
             artifactId = "message-converter"
-            version = "0.0.7"
+            version = "0.0.8"
             from(components["java"])
         }
     }
